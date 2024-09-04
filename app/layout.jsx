b4b145b91a4e06d9b23a400a6b3f60e7b1cf2f71
Stephanie Odoom <styzyl18@gmail.com>
2024-09-04 17:59:12 +0000

    img

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,7 @@ import Navbar from '@components/Navbar'
 import Footer from '@components/Footer'
 
 export const metadata = {
-    metadataBase: new URL('https://steredapharmacy.com'),
+    metadataBase: new URL('https://steredapharmacy.netlify.app/'),
     title: 'Stereda Pharmacy Home: Providing top-quality healthcare services in Ghana.',
     description: 'Stereda Pharmacy Website',
     openGraph: {
